10b34b7b4f76a520bd6b25923c569c20ec4b5ce9
FeSette <felipe_sette@outlook.com>
2025-08-20 15:54:27 -0300

    Entendendo as sintaxes do Python e Blocos

---
+++ Modulo1/sintaxes.py
@@ -0,0 +1,14 @@
+# Adicionar comentários no Python em uma linha, basta utilizar o "Jogo da Velha" antes.
+
+"""
+Para comentários em múltiplas linhas, basta utilizar aspas duplas 3x antes e depois do conteúdo que deve ser comentado
+"""
+
+#blocos
+
+idade = 16
+
+if idade < 18:
+    print("Menor de idade")
+else:
+    print("Maior de idade")
