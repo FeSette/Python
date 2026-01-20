0634a8e248d7aa3601fdc2c44031a4223e0644b1
FeSette <felipe_sette@outlook.com>
2026-01-19 22:48:45 -0300

    Entendendo sobre sintaxe no Python

---
+++ Modulo01/Sintaxe.py
@@ -0,0 +1,21 @@
+# Comentário em 1 linha
+
+"""
+    Comentário 
+    em
+    diversas 
+    linhas    
+"""
+
+# Juntando valores em variaveis em mais de 1 linha
+nome = "Felipe" + \
+" Sette"
+print(nome)
+
+# Compreendendo a identação
+idade = 32
+
+if idade < 18:
+    print("Menor de idade!")
+else:
+    print("Maior de idade!")
