9b92cb74a40eaf7203bdfc2859c1c40514feebde
FeSette <felipe_sette@outlook.com>
2026-01-21 19:24:11 -0300

    Entendendo as variáveis numéricas

---
+++ Modulo01/tiposVariaveis/numeros.py
@@ -0,0 +1,11 @@
+# Inteiro
+numeroInteiro = 3
+print("Inteiro =",numeroInteiro)
+
+# Real com ponto flutuante
+numeroReal = 3.11
+print("Real com ponto flutuante =",numeroReal)
+
+# Type()
+print("Tipo da variavel inteiro =",type(numeroInteiro))
+print("Tipo da variavel real =",type(numeroReal))
