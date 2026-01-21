cdf20084c0c50cac10cc943d597d514182ec06a2
FeSette <felipe_sette@outlook.com>
2026-01-21 19:50:03 -0300

    Entendendo as variáveis numéricas

--- Modulo01/tiposVariaveis/numeros.py
+++ Modulo01/tiposVariaveis/numeros.py
@@ -1,11 +1,41 @@
 # Inteiro
 numeroInteiro = 3
-print("Inteiro =",numeroInteiro)
+print("Inteiro =", numeroInteiro)
 
 # Real com ponto flutuante
 numeroReal = 3.11
-print("Real com ponto flutuante =",numeroReal)
+print("Real com ponto flutuante =", numeroReal)
 
 # Type()
-print("Tipo da variavel inteiro =",type(numeroInteiro))
-print("Tipo da variavel real =",type(numeroReal))
+print("Tipo da variavel inteiro =", type(numeroInteiro))
+print("Tipo da variavel real =", type(numeroReal))
+
+## Operações aritméticas
+
+# Soma (+)
+soma = 1+1
+print("1+1 =", soma)
+
+# Subtração (-)
+subtracao = 1-1
+print("1-1 =", subtracao)
+
+# Multiplicação (*)
+multiplicacao = 2*2
+print("2x2 =", multiplicacao)
+
+# Divisão (/)
+divisao = 5/2
+print("5/2 =", divisao)
+#Qual é o tipo da variável do resultado de uma divisão?
+print("Tipo da variável do resultado da divisão é:", type(divisao))
+# Como transformar uma variável do tipo real/float em tipo inteiro?
+print("Resultado da divisão transformado em inteiro é:", int(divisao)) 
+
+# Módulo (%) - Resto da divisão
+modulo = 5%2
+print("O que resta da divisão de 5/2? =", modulo)
+
+# Divisão com o resultado inteiro (//)
+divInteiro = 5//2
+print("Divisão com o resultado tipo inteiro =", divInteiro)
