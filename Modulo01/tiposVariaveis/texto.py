9c7f99cdb2e0d27ee17c63eaf6ede9e56bcf0cae
FeSette <felipe_sette@outlook.com>
2026-01-22 14:10:29 -0300

    Entendendo as variáveis de texto

---
+++ Modulo01/tiposVariaveis/texto.py
@@ -0,0 +1,13 @@
+# Declaração
+nomeCompleto = "Felipe Sette"
+nome = "Felipe"
+sobrenome = "Sette"
+
+# Declaração com 3x aspas permite a quebra de linha
+nomeCompletoAspas = """Felipe
+Sette"""
+
+# Declaração com quebra de linha usando a \
+nomeCompletoQuebra = "Felipe \
+Sette"
+
