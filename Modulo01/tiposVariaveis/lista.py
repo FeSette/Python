3366e2a9d1f4fb90e0d3bb8fb82f969b9d5dd343
FeSette <felipe_sette@outlook.com>
2026-01-22 19:24:40 -0300

    Entendendo as listas

---
+++ Modulo01/tiposVariaveis/lista.py
@@ -0,0 +1,12 @@
+# Declaração
+minhaLista = [1, 2, 3, 4, 5, "lista", True, False]
+
+# Exibindo a lista
+print(f"minha lista de exemplo: {minhaLista}")
+
+# Exibindo 
+print(f"Meu elemento de índice 5 da minha lista: {minhaLista[5]}")
+print(f"Meus elemento de índice 3 ao 7 da minha lista: {minhaLista[3:7]}")
+print(f"Meu elemento de índice do 0 ao 6 da minha lista: {minhaLista[:6]}")
+print(f"Meu elemento de índice do 0 ao 6 da minha lista: {minhaLista[2:]}")
+
