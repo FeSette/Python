2728c3750cffe503878816138729c7ee111cf2a7
FeSette <felipe_sette@outlook.com>
2026-01-22 19:04:49 -0300

    Entendendo os Booleanos

---
+++ Modulo01/tiposVariaveis/booleanos.py
@@ -0,0 +1,32 @@
+# Booleano tem apenas 2 condições
+# TRUE e FALSE
+
+## Condicionais
+if True:
+    print("Bloco IF será executado!")
+else:
+    print("Bloco ELSE não será executado!")
+
+## Operadores lógicos: 
+# AND (Será executado somente se as duas condições forem verdadeiras)
+if True and True:
+    print("Bloco será executado!")
+
+if True and False:
+    print("Bloco náo será executado!")
+
+if False and False:
+    print("Bloco não será executado!")
+
+#OR (Será executado se uma condção for verdadeira)
+if True or False:
+    print ("Bloco será executado!")
+
+if False or False:
+    print ("Bloco não será executado!")
+
+if True or True:
+    print ("Bloco será executado!")
+
+
+          
