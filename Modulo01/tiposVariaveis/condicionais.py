2cac4c78580653cf736883a1549eeb995b0edcaf
FeSette <felipe_sette@outlook.com>
2026-01-23 17:34:02 -0300

    Entendendo sobre inputs

--- Modulo01/tiposVariaveis/condicionais.py
+++ Modulo01/tiposVariaveis/condicionais.py
@@ -1,15 +1,14 @@
 # if, elif, else
 
-idade = 19
-print("Exemplo de comando if:")
+idade = int(input("Quantos anos você tem?: "))
+
 if idade >= 18:
     print("Você é maior de idade!")
 elif idade >= 12:
-    print("Você é um adolescente")
+    print("Você é um adolescente!")
 else:
     print("você é menor de idade!")
 
 # if, else na mesma linha
-
-mensagem = "Pode tirar a carteira de habilitação" if idade>=18 else "você não pode tirar a carteira de habilitação"
+mensagem = "Você pode tirar a carteira de habilitação!" if idade>=18 else "você não pode tirar a carteira de habilitação!"
 print(mensagem)
