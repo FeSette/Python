ae18ec00f732fb7a533e03b5a74de626a558dea5
FeSette <felipe_sette@outlook.com>
2026-01-23 15:51:00 -0300

    Entendendo os métodos dos dicionários

--- Modulo01/tiposVariaveis/dicionarios.py
+++ Modulo01/tiposVariaveis/dicionarios.py
@@ -13,3 +13,29 @@ print(f"Cidade: {pessoa['cidade']}")
 pessoa["sobrenome"] = "Sette"
 print(f"Meu dicionário: {pessoa}")
 print(f"Sobrenome: {pessoa['sobrenome']}")
+
+## Métodos
+
+# Alterar o valor de uma Key
+pessoa["idade"] = 31
+print(f"Idade atualizada: {pessoa['idade']}")
+
+# Removendo um par chave-valor
+del pessoa["sobrenome"]
+print(f"Meu dicionário: {pessoa}")
+
+# Keys()
+# Tranformar o dicionário em uma lista
+chaves = list(pessoa.keys())
+print(f"Chaves do dicionário: {chaves}")
+print(f"Primeira chave: {chaves[0]}")
+
+# values()
+valores = list(pessoa.values())
+print(f"Valores do dicionário: {valores}")
+print(f"Primeiro valor: {valores[0]}")
+
+# items()
+itens = list(pessoa.items())
+print(f"Pares chave-valor do dicionário: {itens}")
+print(f"Primeira chave-valor: %s = %s" %(itens[0][0], itens[0][1]))
