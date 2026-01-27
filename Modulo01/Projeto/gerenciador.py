bb324f369ccb3ba7b3f34baf04b4752219b0685e
FeSette <felipe_sette@outlook.com>
2026-01-27 16:20:11 -0300

    Iniciando o projeto de lista de tarefas com wuile

---
+++ Modulo01/Projeto/gerenciador.py
@@ -0,0 +1,15 @@
+while True:
+    print("\nMenu do Gerenciador de Lista de Tarefas:")
+    print("1. Adicionar Tarefa")
+    print("2. Ver tarefas")
+    print("3. Atualizar tarefa")
+    print("4. Completar tarefa")
+    print("5. Deletar tarefas completadas")
+    print("6. Sair")
+
+    opcao = input("Digite a opção: ")
+
+    if opcao == "6":
+        break
+
+print("Programa finalizado")
