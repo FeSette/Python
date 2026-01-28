9877c6c350e5a49ca6e7ca23c464405cad18c823
FeSette <felipe_sette@outlook.com>
2026-01-28 13:29:39 -0300

    Entendendo sobre as exceções

---
+++ Modulo01/excecoes.py
@@ -0,0 +1,14 @@
+# Exemplo de Exceções
+print("Exemplo de captura de exceções:")
+try:
+    numero = int(input("Digite um número inteiro: "))
+    resultado = 10/numero
+except ValueError as e:
+    print(f"Erro de ValueError: {e}")
+    raise ValueError("Tipo de variáveis incompatíveis")
+except Exception as e:
+    print(f"Erro: {e}")
+else:
+    print(f"Resultado: {resultado}")
+finally:
+    print("Operação finalizada!")
